0d51540a8600f880ffb8691fb454514605a49b94
Felixdavid12 <felixdavidgom@gmail.com>
2020-12-17 19:32:46 -0500

    Nuevos archivos, trabajo de clase.

--- db/db_connection.py
+++ db/db_connection.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
 # DATABASE_URL = "postgresql://postgres:4561@localhost:5432/MISION_TIC"
 DATABASE_URL = os.environ['DATABASE_URL']
