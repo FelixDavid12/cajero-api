12c3e0f4ebe168c2af24cbf9073101c134d86a07
Felixdavid12 <felixdavidgom@gmail.com>
2020-12-17 20:18:10 -0500

    pool_pre_ping=True

--- db/db_connection.py
+++ db/db_connection.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker
 # DATABASE_URL = "postgresql://postgres:4561@localhost:5432/MISION_TIC"
 # DATABASE_URL = os.environ['DATABASE_URL']
 DATABASE_URL = "postgres://viozdwtzzszddn:a35acb2522bf2b1199d6e97d28452ab1c65dab5c2e177c38b29de8ef8403013f@ec2-54-88-130-244.compute-1.amazonaws.com:5432/d41v2mjf65h6da"
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
